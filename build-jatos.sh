5704f009ad4cfa335b627f46785679722a5c5cb7
Jason Gullifer <jason.gullifer@gmail.com>
2020-06-05 13:38:49 -0400

    updated docker to use volumes

--- build-jatos.sh
+++ build-jatos.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mkdir -p study_assets_root
-mkdir -p study_logs
+
+# Create persistent dirs if empty
+mkdir -p persist/study_assets_root
+mkdir -p persist/study_logs
+mkdir -p persist/database
 
 docker build -t jatos_docker -f "./DockerFile" ./
-docker-compose up
